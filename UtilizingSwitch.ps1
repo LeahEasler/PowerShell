c966e616804ab22cb8d736476b5e06280b8e6685
Leah Easler <leahbeasler@outook.com>
2024-01-07 09:51:22 -0500

    Adding the switch file to the repository.

---
+++ UtilizingSwitch.ps1
@@ -0,0 +1,15 @@
+$currentServer = $servers[0]
+switch ($currentServer) {
+    servers[0]{
+        # Checks to see if SVR1 is online and get content if it is.
+        break
+    }
+    servers[1]{
+        # Does the same for SVR2
+        break
+    }
+    servers[2]{
+        # Does the same for SVR3
+        break
+    }
+}
